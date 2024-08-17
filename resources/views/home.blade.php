8ed18f4e3dee2941434f4be12a8f8b46213b77bf
mhmd <mkma053@gmail.com>
2024-08-17 22:26:33 +0300

    e3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Furniture</title>
+    <title>بانوراما القصيم للبرمجة والتصميم</title>
+    <link rel="icon" type="image/png" href="image/logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="style.css">
     <!-- bootstrap link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -27,12 +29,15 @@
     <div class="container">
         <div class="navbar-top">
             <div class="logo">
-                <h3>بانوراما القصيم </h3>
+                <h3 style="color: #800000">بانوراما القصيم </h3>
             </div>
             <div class="social-link">
-                <i><img src="./image/twitter.png" alt="" width="30px"></i>
-                <i><img src="./image/facebook.png" alt="" width="30px"></i>
-                <i><img src="./image/google-plus.png" alt="" width="30px"></i>
+                <a href="https://x.com/Panorama_Q" style="width: 30px"><i class="fa-brands fa-twitter" ></i></a>
+                <a href="https://www.instagram.com/palqassim/" style="width: 30px"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.tiktok.com/@panorama_alqassim" style="width: 30px"><i class="fa-brands fa-tiktok"></i></a>
+                <a href="https://www.snapchat.com/add/panorama_q" style="width: 30px"><i class="fa-brands fa-snapchat"></i></a>
+                <a href="https://www.facebook.com/palqassim" style="width: 30px"><i class="fa-brands fa-facebook"></i></a>
+
             </div>
 
 
@@ -41,17 +46,18 @@
     <!-- navbar top -->
 
     <!-- main content -->
-    <div class="main-content">
+    <div class="main-content ">
         <nav class="navbar navbar-expand-md" id="navbar-color">
            <div class="container">
             <!-- Toggler/collapsibe Button -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
               <span><i><img src="./image/menu.png" alt="" width="30px"></i></span>
             </button>
+            <img class="" src="image/logo.png" alt="" style="width: 40px; height:50px ">
 
             <!-- Navbar links -->
             <div class="collapse navbar-collapse" id="collapsibleNavbar" style="z-index: 9999;">
-              <ul class="navbar-nav">
+                <ul class="navbar-nav">
                 <li class="nav-item">
                   <a class="nav-link" href="#home">الرئيسية</a>
                 </li>
@@ -76,118 +82,99 @@
           </nav>
 
 
-          <div class="content" id="home" dir="rtl" class="p-10" style="margin-top: 50px;">
+          <div class="content col-md-20 py-3 py-md-0" id="home" dir="rtl" class="p-10" style="margin-top: 150px;">
 
             <h1>
                 بانوراما القصيم للبرمجة والتصميم
             </h1>
-            <h2>
+            <h2 class="col-md-6 py-3 py-md-0">
                 نقدم لعملائنا أفضل الحلول والخدمات الرقميـة المتكامـــــــــــــــلـة
             </h2>
-            <img src="image/logo.png" alt="" style="width: 200px; ">
+            <img class="col-md-6 py-3 py-md-0" src="image/logo2.png" alt="" style="width: 200px; ">
           </div>
 
     </div>
     <!-- main content -->
 
     <!-- about -->
-     <section id="about" style="background-color: #FF9100;">
+     <section id="about" style="background-color: #f4e0c5;">
     <div class="container">
         <h1 class="text-center" style="padding-top: 30px;  color:#800000;">من نحن</h1>
-        <div class="row" style="margin-top: 50px;">
+        <div class="c1" >
+            <img class="text-center col-md-12 py-3 py-md-0" src="image/logo3.png" alt="" style="height: 150px" dir="rtl">
+        <div class=" c row" style="margin-top: 50px; background-color: #ea9322;">
 
-            <div class="col-md-6 py-3 py-md-0">
-                <p style="color: #fffcfc; font-size:20px" dir="rtl">إيمانًا منا بأهمية البرمجة و العالم الرقمي في حياتنا كنا نحن بانوراما القصيم، من الرواد في هذه المجالاتي التي تسعى للارتقاء بمجال التصميم والبرمجة لتكون من كبرى الصروح بالمملكة والشرق الأوسط في تصميم وبرمجة المواقع وتطبيقات الجوال والبرامج الإدارية والمحاسبية .</p>
-            </div>
-            <div class="col-md-6 py-3 py-md-0">
-              <div class="card" style="border-radius: 20px">
-                  <img src="image/logo3.png" alt="" style="width: 100%; ">
-              </div>
-          </div>
-        </div>
-
-        <div class="row" style="margin-top: 50px;">
-
-            <div class="col-md-6 py-3 py-md-0">
-                <p style="color: #fffcfc; font-size:20px" dir="rtl">
-                    نهدف دائمًا لإرضاء كافة العملاء لدينا من خلال تقديم مجموعة من الخدمات المميزة. نسعى لخلق وتنمية جسور التعاون والشراكة وتبادل الخبرات بين المبرمجين في مختلف تخصصاتهم، ونستهدف إعداد برامج متميزة تلبي الاحتياجات الحقيقية للمجتمع وسوق العمل مع تطويرها المستمر. ندرب المبرمجين حديثي التخرج لنرتقي بـالمستويات المهارية في جميع مجالات البرمجة من الأنشطة الأساسية لخدمة المجتمع المدني.</p>
-            </div>
-            <div class="col-md-6 py-3 py-md-0">
-              <div class="card" style="border-radius: 20px">
-                  <img src="image/logo3.png" alt="" style="width: 100%; ">
-              </div>
-          </div>
-        </div>
-
-        <div class="row" style="margin-top: 50px;">
-
-            <div class="col-md-6 py-3 py-md-0">
-                <p style="color: #fffcfc; font-size:20px" dir="rtl">
-                    رسالتنا هي خلق مجتمع واعِ فكريًا و متطور علميًا، قادرًا على مواكبة العالم الرقمي بأسهل الطرق وأبسطها.
+            <div class="col-md-12 py-3 py-md-0">
+                <p style="font-size: 35px"><b>نؤمن</b></p>
+                <p style="color: #fffcfc; font-size:20px" dir="rtl">بأهمية البرمجة و العالم الرقمي في حياتنا لذلك كنا نحن بانوراما القصيم، من الرواد في هذه المجالات التي تسعى للارتقاء بمجال التصميم والبرمجة لتكون من كبرى الصروح بالمملكة والشرق الأوسط في تصميم وبرمجة المواقع وتطبيقات الجوال والبرامج الإدارية والمحاسبية .
                 </p>
             </div>
-            <div class="col-md-6 py-3 py-md-0">
-              <div class="card" style="border-radius: 20px">
-                  <img src="image/logo3.png" alt="" style="width: 100%; ">
-              </div>
-          </div>
+
         </div>
 
+        <div class="c row" style="margin-top: 50px; background-color: #f49212;">
 
+            <div class="col-md-12 py-3 py-md-0">
+                <p style="font-size: 35px"><b>نهدف</b></p>
+                <p style="color: #fffcfc; font-size:20px" dir="rtl">
+                    لإرضاء كافة العملاء لدينا من خلال تقديم مجموعة من الخدمات المميزة. نسعى لخلق وتنمية جسور التعاون والشراكة وتبادل الخبرات بين المبرمجين في مختلف تخصصاتهم، ونستهدف إعداد برامج متميزة تلبي الاحتياجات الحقيقية للمجتمع وسوق العمل مع تطويرها المستمر. ندرب المبرمجين حديثي التخرج لنرتقي بـالمستويات المهارية في جميع مجالات البرمجة من الأنشطة الأساسية لخدمة المجتمع المدني.
+                </p>
+           </div>
+
+        </div>
+
+        <div class="c row col-md-12 py-3 py-md-0" style="margin-top: 50px; background-color: #ed5d15;">
+
+            <div class="col-md-12 py-3 py-md-0">
+                <p style="font-size: 35px"><b>رسالتنا</b></p>
+                <p style="color: #fffcfc; font-size:20px" dir="rtl">
+                  خلق مجتمع واعِ فكريًا و متطور علميًا، قادرًا على مواكبة العالم الرقمي بأسهل الطرق وأبسطها.
+                </p>
+            </div>
+
+        </div>
+
+    </div>
     </div>
 </section>
     <!-- about -->
 
     <!-- card1 -->
 
-     <section style="background-color: #800000; padding: 5px;" id="services">
+     <section style="background: linear-gradient(0deg, #f1f1f1 3%, #991515 96%); padding: 5px;" id="services">
 
         <div class="container" style="padding: 5px;">
         <h3 class="text-center" style="padding-top: 30px; color:#FF9100">خدماتنا</h3>
         @foreach ($serv as $serr)
-        <div class="row " style="margin-top: 30px;">
-
-            <div class=" card col-md-6 py-3 py-md-0">
-
-                <div class="card" style=" ">
-
-                        <h5 class=" text-center"> {{$serr->title}} </h5>
-                        <img src="../{{$serr->img}}" alt="" class="card image-top" height="200px">
-                        <p class="text-center">{{$serr->description}} </p>
-
-                </div>
-
+        <div class=" row " style="margin-top: 30px; border-radius: 35px; box-shadow: 0 5px 25px; padding: 20px; margin: 20px auto; background-color:#FF9100">
+            <div class=" col-md-12 py-3 py-md-0" style="">
+                <h5 class=" text-center"> {{$serr->title}} </h5>
+                <img src="../{{$serr->img}}" alt="" class=" col-md-12 py-3 py-md-0" height="200px" style="margin-top: 15px;">
+                <p class="text-center" style="margin-top: 15px">{{$serr->description}} </p>
             </div>
-
         </div>
         @endforeach
     </div>
-
 </section>
 
-    <!-- card1 -->
-
-
-
-    <!-- card3 -->
-    <section style="background-color:#FF9100; padding: 5px;" id="costmer">
+    <section style="background: linear-gradient(0deg, #f1f1f1 3%, #FF9100 96%); padding: 5px;" id="costmer">
         <div class="container" style="padding: 5px;">
             <h3 class="text-center" style="padding-top: 30px; color:#800000">عملائنا</h3>
-            <div class="row" style="margin-top: 30px auto;">
+            <div class="row" style="margin-top: 30px; border-radius: 35px; box-shadow: 0 5px 25px; padding: 20px; margin: 20px auto; background-color:#800000">
                 @foreach ($cli as $cl)
-                <div class="col-md-5 py-3 py-md-0">
-                    <div class="card" style="background-color:#800000; color: #fffcfc;">
+                <div class="col-md-12 py-3 py-md-0">
+
                             <h5 class=" text-center"> {{$cl->title}} </h5>
-                            <img src="../{{$cl->img}}" alt="" class="card image-top" height="200px">
-                            <p class="text-center">{{$cl->description}} </p>
-                    </div>
+                            <img src="../{{$cl->img}}" alt="" class=" col-md-12 py-3 py-md-0" height="200px" style="margin-top: 15px;">
+                            <p class="text-center" style="margin-top: 15px">{{$cl->description}} </p>
+
                 </div>
                 @endforeach
             </div>
         </div>
     </section>
 
-    <section class="contact" style="background-color:#800000;" id="contact" dir="rtl">
+    <section class="contact" style="background: linear-gradient(0deg, #800000 3%, #f1f1f1 97%);" id="contact" dir="rtl">
 
           <div class="container">
             <div class="container contact-form">
@@ -199,20 +186,21 @@
                     <h3 >اترك لنا رسالتك </h3>
                    <div class="row">
                         <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control" placeholder="الاسم *" value="" />
+                            <div class="form-group" >
+                                <input type="text" name="name" class="form-control" style="width: 100%; height: 50px;" placeholder="الاسم *" value="" />
                             </div>
-                            <div class="form-group">
-                                <input type="text" name="email" class="form-control" placeholder="البريد الالكتروني *" value="" />
+                            <div class="form-group" style="padding-top: 20px">
+                                <input type="text" name="email" class="form-control" style="width: 100%; height: 50px;" placeholder="البريد الالكتروني *" value="" />
                             </div>
-                            <div class="form-group">
-                                <input type="submit" name="" class="btnContact" value="إرسال" />
-                            </div>
+
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <textarea name="msg" class="form-control" placeholder="الرسالة *" style="width: 100%; height: 150px;"></textarea>
                             </div>
+                        </div>
+                        <div class="form-group col-md-4 align-self-end" dir="rtl" >
+                            <input type="submit" name="" class="btnContact" value="إرسال" />
                         </div>
                     </div>
                 </form>
@@ -222,11 +210,12 @@
     <footer id="footer" style="background-color:#FF9100;">
         <h1 class="text-center">بانوراما القصيم للبرمجة والتصميم </h1>
         <div class="icons text-center">
-            <i class="bx bxl-twitter"></i>
-            <i class="bx bxl-facebook"></i>
-            <i class="bx bxl-google"></i>
-            <i class="bx bxl-skype"></i>
-            <i class="bx bxl-instagram"></i>
+            <a href="https://x.com/Panorama_Q" style="width: 30px"><i class="fa-brands fa-twitter" ></i></a>
+                <a href="https://www.instagram.com/palqassim/" style="width: 30px"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.tiktok.com/@panorama_alqassim" style="width: 30px"><i class="fa-brands fa-tiktok"></i></a>
+                <a href="https://www.snapchat.com/add/panorama_q" style="width: 30px"><i class="fa-brands fa-snapchat"></i></a>
+                <a href="https://www.facebook.com/palqassim" style="width: 30px"><i class="fa-brands fa-facebook"></i></a>
+
         </div>
         <div class="copyright text-center">
             &copy; تم التصميم <strong></strong>بواسطة بانوراما القصيم.
